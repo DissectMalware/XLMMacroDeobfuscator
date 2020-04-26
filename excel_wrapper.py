9e2a370a37b605a487351606ef1e50d9a59e3df9
aniakanl <aniakan@gmail.com>
2020-04-26 19:06:21 -0400

    Update excel_wrapper.py

--- excel_wrapper.py
+++ excel_wrapper.py
@@ -1,6 +1,10 @@
 class ExcelWrapper:
+    def get_defined_name(self, name, full_match):
+        pass
+
     def get_defined_names(self):
         pass
 
     def get_macrosheets(self):
         pass
+
