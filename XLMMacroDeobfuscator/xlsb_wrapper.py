292dec0198768beb78e9a854decd6eab88a34b26
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2021-12-08 00:51:23 +0000

    REVERT - changes for shared string as it is covered in pyxlsb v0.0.8

--- XLMMacroDeobfuscator/xlsb_wrapper.py
+++ XLMMacroDeobfuscator/xlsb_wrapper.py
@@ -60,10 +60,7 @@ class XLSBWrapper(ExcelWrapper):
                     tmp_cell = Cell()
                     tmp_cell.row = cell.row_num + 1
                     tmp_cell.column = Cell.convert_to_column_name(cell.col + 1)
-                    if cell.style_id == 1 and cell.formula is None and shared_table and isinstance(cell.value, int) :
-                        tmp_cell.value = shared_table.get_string(cell.value)
-                    else:
-                        tmp_cell.value = cell.value
+                    tmp_cell.value = cell.value
                     tmp_cell.sheet = boundsheet
                     formula_str = Formula.parse(cell.formula)
                     if formula_str._tokens:
